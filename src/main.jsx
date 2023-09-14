f9ac802677d9e821ddf2eb2cacb5b1d051c4ac67
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-09-13 21:09:02 -0700

    Try moving to main instead of app

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Helmet>
+      <title>Hydraulic Fracturing in British Columbia</title>
+      <meta
+        name="description"
+        content="An educational website to learn React"
+      />
+      <script
+        defer
+        data-domain="understandingafracking.site"
+        src="https://analytics.markpitblado.me/js/script.js"
+      ></script>
+    </Helmet>
     <App />
   </BrowserRouter>
 );
